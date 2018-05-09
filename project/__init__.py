8f4691a0e92f934e9aaf9f03b2538af984d5556c
sharabesh <sharabeshjayaraman@gmail.com>
2018-05-09 23:26:56 +0530

    added flask migrate as wrapper to include alembic

--- project/__init__.py
+++ project/__init__.py
@@ -1,12 +1,14 @@
 #imports
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 #config
 app = Flask(__name__,instance_relative_config=True)
 app.config.from_pyfile('flask.cfg')
 
 db = SQLAlchemy(app) 
+migrate = Migrate(app,db)
 
 #blueprints 
 from project.users.views import users_blueprint
--- project/models.py
+++ project/models.py
@@ -17,3 +17,17 @@ class Expense(db.Model):
 
     def __repr__(self):
         return 'id {}'.format(self.id)
+
+class User(db.Model):
+    __tablename__ = "users"
+    
+    id = db.Column(db.Integer,primary_key = True)
+    email = db.Column(db.String,unique = True, nullable = False)
+    password = db.Column(db.String,nullable = False)
+
+    def __init__(self,email,password):
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return 'User {}'.format(self.)
