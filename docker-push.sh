af9c464adb39263daa21f0d3060f47de00cc289a
sharabeshj <sharabeshjayaraman@gmail.com>
2018-06-25 17:49:58 +0530

    first in staf

--- docker-push.sh
+++ docker-push.sh
@@ -10,7 +10,9 @@ then
         unzip awscli-bundle.zip
         ./awscli-bundle/install -b ~/bin/aws
     export PATH=~/bin:$PATH
-    
+    export AWS_ACCOUNT_ID=467016297208
+    export AWS_ACCESS_KEY_ID=AKIAJE2YFQ37KMS25VJQ
+    export AWS_SECRET_ACCESS_KEY=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
     eval $(aws ecr get-login --region us-east-2 --no-include-email)
     export TAG=$TRAVIS_BRANCH
     export REPO=$AWS_ACCOUNT_ID.dkr.ecr.us-east-2.amazonaws.com
@@ -20,7 +22,7 @@ then
        ["$TRAVIS_BRANCH" == "production"] 
     then
         #users
-        docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-prod
+        docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV
         docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
         docker push $REPO/$USERS:$TAG
         #users_db
@@ -28,11 +30,11 @@ then
         docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
         docker push $REPO/$USERS_DB:$TAG
         #client
-        docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-prod --build-arg REACT_APP_USERS_SERVICE_URL=TBD
+        docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build-arg REACT_APP_USERS_SERVICE_URL=TBD
         docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
         docker push $REPO/$CLIENT:$TAG
         #swagger
-        docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-prod $SWAGGER_DIR
+        docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-$DOCKER_ENV $SWAGGER_DIR
         docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
         docker push $REPO/$SWAGGER:$TAG
     fi
